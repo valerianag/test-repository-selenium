d219fc6fbdf8092af8997cab226cbebe13eea83d
valerianag <v_nag@bk.ru>
2017-04-27 17:19:37 +0300

    Задание 7
    Кол-во заголовков пишется в логи в параметре counter

--- myFirstSeleniumTest/src/test/java/testFirst.java
+++ myFirstSeleniumTest/src/test/java/testFirst.java
@@ -3,11 +3,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -24,9 +28,12 @@ public class testFirst {
     private WebDriverWait wait;
 
 
+
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -38,7 +45,53 @@ public class testFirst {
         driver.findElement(By.name("password")).sendKeys("admin");
 
         driver.findElement(By.tagName("button")).click();
+
+        //не нашла условия для достаточного Implicitely Wait
         Thread.sleep(5000);
+       // driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+        List<WebElement> links = driver.findElements(By.id("app-"));
+        openMenu(links, driver);
+
+        //здесь просто для наглядности
+        Thread.sleep(2000);
+        driver.navigate().back();
+
+    }
+
+    public void openMenu(List<WebElement> l, WebDriver dr) throws InterruptedException {
+        List<String> text =new LinkedList();
+        //лист всех менюшек
+        for (int i=0; i<l.size(); i++) {
+            text.add(l.get(i).getText());
+        }
+
+        //заходим в меню
+        int counter=0; //сколько заголовков
+        for (int i=0; i<text.size(); i++) {
+            dr.findElement(By.partialLinkText(text.get(i))).click();
+            List<String> txt = new LinkedList<String>();
+            List<WebElement> menu = dr.findElements(By.xpath("//li[@class='selected']//span[@class='name']"));
+
+
+            //ищем подменю
+            for (int j=1; j<menu.size(); j++) {
+
+                      txt.add(menu.get(j).getText());
+
+            }
+            for (int j=0; j<txt.size(); j++) {
+                dr.findElement(By.partialLinkText(txt.get(j))).click();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
+               // Thread.sleep(500);
+                if ((dr.findElement(By.tagName("h1")))!=null)
+                    counter++;
+               // driver.navigate().back();
+            }
+
+            driver.navigate().back();
+        }
+
+        System.out.println("Найдено заголовков: "+counter);
     }
 
 
