c80187fec15a3ad423d7e6318dcd2668accab86c
valerianag <v_nag@bk.ru>
2017-04-27 09:55:16 +0300

    Задание 1
    Исправлен сценарий логина

--- myFirstSeleniumTest/src/test/java/testFirst.java
+++ myFirstSeleniumTest/src/test/java/testFirst.java
@@ -4,39 +4,50 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 
 /**
  * Created by User16_06_03 on 18.04.2017.
  */
 public class testFirst {
 
-   private WebDriver driver;
-   private WebDriverWait wait;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
-   @Before
+
+    @Before
     public void start() {
-       driver = new ChromeDriver();
-       wait = new WebDriverWait(driver, 10);
-   }
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
 
-   @Test
-    public void testFirst() {
-              driver.get("http://www.google.com/");
-              driver.findElement(By.name("q")).sendKeys("webdriver");
-              driver.findElement(By.name("btnG")).click();
-              wait.until(titleIs("webdriver - Поиск в Google"));
-   }
+    @Test
+    public void testFirst() throws InterruptedException {
+        driver.get("http://localhost/litecart/admin/");
+
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(5000);
+    }
 
 
 
-       @After
-               public void stop() {
-           driver.quit();
-           driver = null;
-       }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
    }
 
